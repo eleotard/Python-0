6598e21f975de0b616c3722f93389fe5da3be897
elsie <eleotard@student.42.fr>
2024-03-25 16:57:20 +0100

    ex01 fini

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,17 +1,19 @@
 import time
+from datetime import date
 
 t = time.time() #float
 scientific_notation = f"{t:.2e}"
 
 t = int(round(t, 4) * 10_000)
+# print(t)
 
 md = t // 10**13
 ml = (t - md * 10**13) // 10**10
 mi = (t - md * 10**13 - ml * 10**10) // 10**7
-ce = (t - md * 10**13 - ml * 10**10 - mi * 10**3) // 10**4
+ce = (t - md * 10**13 - ml * 10**10 - mi * 10**7) // 10**4
+de = (t - md * 10**13 - ml * 10**10 - mi * 10**7 - ce * 10**4)
+print(f"Seconds since January 1, 1970: {md},{ml},{mi},{ce}.{de} or {scientific_notation} in scientific notation")
 
-print(t)
-# time_str = str(t)
-# a = ["lol", "danil", "elsie"]
-# print(time_str[1:4])
-print(f"Seconds since January 1, 1970: {md}, {ml}, {mi}, {ce}. {} or {scientific_notation} in scientific notation") 
+date = date.today()
+month = date.ctime()[4:7:]
+print(f"{month} {date.day} {date.year}")

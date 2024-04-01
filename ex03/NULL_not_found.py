ae9beaf1bd2d8bdd29267e654cd2d1661400b431
elsie <eleotard@student.42.fr>
2024-04-01 16:57:47 +0200

    ex03 fait

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,8 +1,13 @@
 def NULL_not_found(object: any) -> int:
+	d = { "NoneType" : "Nothing", 
+	"float" : "Cheese",
+	"int" : "Zero",
+	"str" : "Empty",
+	"bool" : "Fake"
+	}
 	res = type(object)
-	if res.__name__ == "str":
-		if len(object):
-			print("Type not found")
-			return 1
-	print(f"{res.__name__.title()} : {res}")
+	if (object and object == object) or res.__name__ not in d.keys():
+		print("Type not Found")
+		return 1
+	print(f"{d[res.__name__]}: {object} {res}")
 	return 0

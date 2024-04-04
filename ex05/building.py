b377dc799b751c026638e72f30545e95f4c340c2
Elsie LEOTARD <eleotard@paul-f4Ar5s8.clusters.42paris.fr>
2024-04-04 20:52:44 +0200

    ex07 done

--- ex05/building.py
+++ ex05/building.py
@@ -1,28 +1,44 @@
 import sys
 
+def test(var):
+	var +=1
+	return var
+
 def main():
-	"""cest le main ici"""
+	"""program that takes a string as an argument and print its caracteristics"""
 
 	try:
 		assert len(sys.argv) <= 2
 	except:
 		print("Wrong number of arguments")
+		return 1
 
+	upper = lower = space = digit = ponct = 0
+	ponctuations = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 	s = None if len(sys.argv) == 1 else sys.argv[1]
 	while not s:
 		s = input()
-
+	for c in s:
+		if c.isupper():
+			upper += 1
+		elif c.islower():
+			lower += 1
+		elif c.isdigit():
+			digit += 1
+		elif c.isspace():
+			space += 1
+		elif c in ponctuations:
+			ponct += 1
+	print(f"The text contains {len(s)} characters:")
+	print(f"{lower} lower letters")
+	print(f"{upper} upper letters")
+	print(f"{ponct} ponctuation marks")
+	print(f"{space} spaces")
+	print(f"{digit} digits")
+	return 0
 	
-
-	# print([i.isupper() for i in s])
-
-	# if len(sys.argv) == 1 or not sys.argv[1]:
-	# 	s = input()
-	# 	while not s:
-	# 		s = input()
-	# else:
-	# 	s = sys.argv[1]
-
-
 if __name__ == "__main__":
 	main()
+
+# i.isupper() for i in string
+# 1 for c in message if c.isupper()
---
+++ ex07/sos.py
@@ -0,0 +1,30 @@
+import sys
+
+def main():
+	try:
+		assert len(sys.argv) == 2
+		s = str(sys.argv[1])
+		for c in s:
+			assert c.isalpha() or c.isspace()	
+	except:
+		print("AssertionError: the arguments are bad")
+		return 1
+
+	MORSE_CODE_DICT = { ' ' : '/', 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..' }
+	
+	s = s.upper()
+	for i in s:
+		if i in MORSE_CODE_DICT:
+			print(f"{MORSE_CODE_DICT[i]}", end='')
+	return 0
+
+if __name__ == "__main__":
+	main()
